e456c26f126ea07b78ac9e1202f89d303065f584
nuochong <nuo.chong@163.com>
2019-11-04 21:42:35 +0800

    update

--- src/view/WeekHot.js
+++ src/view/WeekHot.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
-import '../assets/css/Article.scss';
+import '../assets/css/WeekHot.scss';
 import Footer from '../components/Footer';
-import Header from '../components/Header';
-import Recommend from '../components/Recommend';
-import Comment from '../components/Comment';
-import ArticleBody from '../components/ArticleBody';
-import Logo from '../components/Logo';
-import ArticleDirectory from '../components/ArticleDirectory';
 
 class Article extends Component {
   constructor() {
@@ -17,16 +11,286 @@ class Article extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <ArticleDirectory/>
-        <ArticleBody />
-        <hr />
-        <Comment />
-        <hr />
-        <Recommend />
-        <hr />
-        <Logo />
-        <Footer />
+        <div id="bargain-books">
+          <div class="banner"></div>
+          <ul class="bargain-books-list">
+            <li>
+              <a href="/nb/15911166" class="book">
+                <img
+                  src="https://upload.jianshu.io/book/image/f5fece84-458b-43a2-83b5-116e9396d40c?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/160/format/webp"
+                  alt="情事侦缉档案"
+                  class="cover"
+                />
+                <div class="btn">
+                  马上抢<i class="iconfont ic-link iconleft"></i>
+                </div>
+                <div class="name">情事侦缉档案</div>
+                <div class="intro">
+                  <i class="iconfont ic-tag icontag"></i>
+                  <span class="category">悬疑</span>
+                  <span class="category">都市</span>
+                  <span class="category">玄幻</span>
+                  <span class="category">情感</span>
+                </div>
+                <div class="price">
+                  <span class="retail-price">9.90</span>
+                  <span class="original-price">19.90</span>
+                </div>
+                <span class="subscribers">278人感兴趣</span>
+              </a>
+            </li>
+            <li>
+              <a href="/nb/24036690" class="book">
+                <img
+                  src="https://upload.jianshu.io/book/image/3d3c1415-a226-4b93-8a5e-ede1776cb5b9?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/160/format/webp"
+                  alt="渡过：抑郁症治愈笔记"
+                  class="cover"
+                />
+                <div class="btn">
+                  马上抢<i class="iconfont ic-link iconleft"></i>
+                </div>
+                <div class="name">渡过：抑郁症治愈笔记</div>
+                <div class="intro">
+                  <i class="iconfont ic-tag icontag"></i>
+                  <span class="category">心理</span>
+                  <span class="category">科普</span>
+                  <span class="category">生活</span>
+                  <span class="category">付费连载</span>
+                  <span class="category">知识</span>
+                </div>
+                <div class="price">
+                  <span class="retail-price">10.00</span>
+                  <span class="original-price">26.90</span>
+                </div>
+                <span class="subscribers">383人感兴趣</span>
+              </a>
+            </li>
+            <li>
+              <a href="/nb/26022672" class="book">
+                <img
+                  src="https://upload.jianshu.io/book/image/46f5022e-100c-4fa3-94f3-a3f681b68bbc?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/160/format/webp"
+                  alt="渡过2：接纳是最好的治愈"
+                  class="cover"
+                />
+                <div class="btn">
+                  马上抢<i class="iconfont ic-link iconleft"></i>
+                </div>
+                <div class="name">渡过2：接纳是最好的治愈</div>
+                <div class="intro">
+                  <i class="iconfont ic-tag icontag"></i>
+                  <span class="category">心理</span>
+                  <span class="category">成长</span>
+                  <span class="category">付费连载</span>
+                  <span class="category">积极心理</span>
+                </div>
+                <div class="price">
+                  <span class="retail-price">10.00</span>
+                  <span class="original-price">26.90</span>
+                </div>
+                <span class="subscribers">121人感兴趣</span>
+              </a>
+            </li>
+            <li>
+              <a href="/nb/26229968" class="book">
+                <img
+                  src="https://upload.jianshu.io/book/image/92c266ba-d39e-4470-9b17-129e01522208?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/160/format/webp"
+                  alt="表达模型30讲 | 有效训练你的沟通表达能力"
+                  class="cover"
+                />
+                <div class="btn">
+                  马上抢<i class="iconfont ic-link iconleft"></i>
+                </div>
+                <div class="name">表达模型30讲 | 有效训练你的沟通表达能力</div>
+                <div class="intro">
+                  <i class="iconfont ic-tag icontag"></i>
+                  <span class="category">职场</span>
+                  <span class="category">成长</span>
+                  <span class="category">演讲与口才</span>
+                  <span class="category">表达</span>
+                  <span class="category">精进课</span>
+                </div>
+                <div class="price">
+                  <span class="retail-price">79.00</span>
+                  <span class="original-price">99.00</span>
+                </div>
+                <span class="subscribers">526人感兴趣</span>
+              </a>
+            </li>
+            <li>
+              <a href="/nb/27284174" class="book">
+                <img
+                  src="https://upload.jianshu.io/book/image/3b334af9-d812-4bee-992d-45c84fcb6e07?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/160/format/webp"
+                  alt="2-8岁宝宝如何学英语——新东方老师的独家英语启蒙课"
+                  class="cover"
+                />
+                <div class="btn">
+                  马上抢<i class="iconfont ic-link iconleft"></i>
+                </div>
+                <div class="name">2-8岁宝宝如何学英语——新东方老师的独家英语启蒙课</div>
+                <div class="intro">
+                  <i class="iconfont ic-tag icontag"></i>
+                  <span class="category">英语学习</span>
+                  <span class="category">精进课</span>
+                </div>
+                <div class="price">
+                  <span class="retail-price">59.00</span>
+                  <span class="original-price">99.00</span>
+                </div>
+                <span class="subscribers">202人感兴趣</span>
+              </a>
+            </li>
+            <li>
+              <a href="/nb/27925733" class="book">
+                <img
+                  src="https://upload.jianshu.io/book/image/5ae80a1a-b3dc-42dc-b74e-d52f9bd40c6b?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/160/format/webp"
+                  alt="职场第1课"
+                  class="cover"
+                />
+                <div class="btn">
+                  马上抢<i class="iconfont ic-link iconleft"></i>
+                </div>
+                <div class="name">职场第1课</div>
+                <div class="intro">
+                  <i class="iconfont ic-tag icontag"></i>
+                  <span class="category">职场</span>
+                  <span class="category">成长</span>
+                  <span class="category">认知</span>
+                  <span class="category">知识</span>
+                  <span class="category">精进课</span>
+                  <span class="category">合集</span>
+                </div>
+                <div class="price">
+                  <span class="retail-price">49.00</span>
+                  <span class="original-price">59.00</span>
+                </div>
+                <span class="subscribers">205人感兴趣</span>
+              </a>
+            </li>
+            <li>
+              <a href="/nb/28370799" class="book">
+                <img
+                  src="https://upload.jianshu.io/book/image/57cdfe5e-cbeb-40cf-8f7a-22c89f4e0547?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/160/format/webp"
+                  alt="掌握说话的力量"
+                  class="cover"
+                />
+                <div class="btn">
+                  马上抢<i class="iconfont ic-link iconleft"></i>
+                </div>
+                <div class="name">掌握说话的力量</div>
+                <div class="intro">
+                  <i class="iconfont ic-tag icontag"></i>
+                  <span class="category">演讲与口才</span>
+                  <span class="category">精进课</span>
+                </div>
+                <div class="price">
+                  <span class="retail-price">19.90</span>
+                  <span class="original-price">69.00</span>
+                </div>
+                <span class="subscribers">1912人感兴趣</span>
+              </a>
+            </li>
+            <li>
+              <a href="/nb/28588571" class="book">
+                <img
+                  src="https://upload.jianshu.io/book/image/3578fc95-5f46-4ebf-a56b-fca13d6b3928?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/160/format/webp"
+                  alt="12堂脑洞大开的思维升级课"
+                  class="cover"
+                />
+                <div class="btn">
+                  马上抢<i class="iconfont ic-link iconleft"></i>
+                </div>
+                <div class="name">12堂脑洞大开的思维升级课</div>
+                <div class="intro">
+                  <i class="iconfont ic-tag icontag"></i>
+                  <span class="category">逻辑与思维</span>
+                  <span class="category">精进课</span>
+                </div>
+                <div class="price">
+                  <span class="retail-price">39.00</span>
+                  <span class="original-price">69.00</span>
+                </div>
+                <span class="subscribers">1831人感兴趣</span>
+              </a>
+            </li>
+            <li>
+              <a href="/nb/28989239" class="book">
+                <img
+                  src="https://upload.jianshu.io/book/image/e6cac7d9-cd13-466e-8f9e-0bfcedd9beba?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/160/format/webp"
+                  alt="《金瓶梅》读什么？——一段生动的明代市井生活"
+                  class="cover"
+                />
+                <div class="btn">
+                  马上抢<i class="iconfont ic-link iconleft"></i>
+                </div>
+                <div class="name">《金瓶梅》读什么？——一段生动的明代市井生活</div>
+                <div class="intro">
+                  <i class="iconfont ic-tag icontag"></i>
+                  <span class="category">历史</span>
+                  <span class="category">读书</span>
+                  <span class="category">文化课</span>
+                  <span class="category">合集</span>
+                </div>
+                <div class="price">
+                  <span class="retail-price">49.00</span>
+                  <span class="original-price">99.00</span>
+                </div>
+                <span class="subscribers">330人感兴趣</span>
+              </a>
+            </li>
+            <li>
+              <a href="/nb/29773446" class="book">
+                <img
+                  src="https://upload.jianshu.io/book/image/93c2855b-6ae3-4e99-b223-43d007cdd4b7.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/160/format/webp"
+                  alt="后背留给我（有声书）"
+                  class="cover"
+                />
+                <div class="btn">
+                  马上抢<i class="iconfont ic-link iconleft"></i>
+                </div>
+                <div class="name">后背留给我（有声书）</div>
+                <div class="intro">
+                  <i class="iconfont ic-tag icontag"></i>
+                  <span class="category">军旅</span>
+                  <span class="category">青春</span>
+                  <span class="category">言情</span>
+                  <span class="category">校园</span>
+                  <span class="category">付费连载</span>
+                  <span class="category">行距</span>
+                </div>
+                <div class="price">
+                  <span class="retail-price">19.90</span>
+                  <span class="original-price">25.00</span>
+                </div>
+                <span class="subscribers">41人感兴趣</span>
+              </a>
+            </li>
+            <li>
+              <a href="/nb/29835098" class="book">
+                <img
+                  src="https://upload.jianshu.io/book/image/a2daa9b0-a2c0-4b08-9e45-84f0581633ab?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/160/format/webp"
+                  alt="简书七大主题征文S1获奖作品音频"
+                  class="cover"
+                />
+                <div class="btn">
+                  马上抢<i class="iconfont ic-link iconleft"></i>
+                </div>
+                <div class="name">简书七大主题征文S1获奖作品音频</div>
+                <div class="intro">
+                  <i class="iconfont ic-tag icontag"></i>
+                  <span class="category">悬疑</span>
+                  <span class="category">短篇集</span>
+                  <span class="category">故事</span>
+                  <span class="category">合集</span>
+                </div>
+                <div class="price">
+                  <span class="retail-price">4.99</span>
+                  <span class="original-price">12.00</span>
+                </div>
+                <span class="subscribers">184人感兴趣</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
