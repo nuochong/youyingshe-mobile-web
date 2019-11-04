fa7f4b34646db40737ebba278fddfcf3d3fc6220
nuochong <nuo.chong@163.com>
2019-11-04 22:09:51 +0800

    update

--- src/view/SpecialOffer.js
+++ src/view/SpecialOffer.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
-import '../assets/css/Article.scss';
+import '../assets/css/SpecialOffer.scss';
 import Footer from '../components/Footer';
-import Header from '../components/Header';
-import Recommend from '../components/Recommend';
-import Comment from '../components/Comment';
-import ArticleBody from '../components/ArticleBody';
-import Logo from '../components/Logo';
-import ArticleDirectory from '../components/ArticleDirectory';
 
 class Article extends Component {
   constructor() {
@@ -17,16 +11,364 @@ class Article extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <ArticleDirectory/>
-        <ArticleBody />
-        <hr />
-        <Comment />
-        <hr />
-        <Recommend />
-        <hr />
-        <Logo />
-        <Footer />
+        <div id="books-weekly-trending" class="books-trending">
+          <div class="books-trending">
+            <div class="banner weekly"></div>
+            <ol>
+              <li>
+                <a href="/nb/240879" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/24a1d492-a669-44ce-9ffe-a325984d6ea1" class="cover" />
+                  <div class="name">末路狂花</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">故事</span>
+                      <span class="category">校园</span>
+                      <span class="category">合集</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      蔡不帅
+                    </span>
+                  </div>
+                  <div class="intro twoline">冷暖自知。</div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/1172116" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/0508d6de-6d18-41af-a823-d9be9a72c425.jpg" class="cover" />
+                  <div class="name">原味法餐</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">美食</span>
+                      <span class="category">生活</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      卢璐说
+                    </span>
+                  </div>
+                  <div class="intro twoline"></div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/3298922" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/2435e42a-25d6-4be6-9446-739335028811" class="cover" />
+                  <div class="name">历史</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">历史</span>
+                      <span class="category">合集</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      丧心病狂刘老湿
+                    </span>
+                  </div>
+                  <div class="intro twoline"></div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/4006354" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/502ace55-1229-4b7b-a037-5d04d69e65f3" class="cover" />
+                  <div class="name">青青美厨</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">美食</span>
+                      <span class="category">生活</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      子莯青青
+                    </span>
+                  </div>
+                  <div class="intro twoline">好好吃饭，天天向上。欢迎关注青青美厨，公众号（qingqingmeichu），致力于营养美食，健康养生。</div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/5254068" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/3c89de1d-9013-4bd9-ad24-64b3546266c8.jpg" class="cover" />
+                  <div class="name">怀左的蜗牛日记</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">成长</span>
+                      <span class="category">合集</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      怀左同学
+                    </span>
+                  </div>
+                  <div class="intro twoline"></div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/5293130" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/441b7213-6fcd-4143-b615-431f758d548e.jpg" class="cover" />
+                  <div class="name">青青外语</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">职场</span>
+                      <span class="category">成长</span>
+                      <span class="category">英语学习</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      子莯青青
+                    </span>
+                  </div>
+                  <div class="intro twoline">分享《老友记》的详细解析；分享最实用的学习方法和单词记忆方法。公众号 青青外语（qingqingwaiyu）</div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/6552291" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/f3bfeda8-f704-401f-93ab-564732fd8dfc" class="cover" />
+                  <div class="name">西达雅的阅读经——书评与读书笔记</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">书评</span>
+                      <span class="category">读书</span>
+                      <span class="category">文学</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      西达雅
+                    </span>
+                  </div>
+                  <div class="intro twoline">用读书扩展生命的宽度，愿你我都能爱上阅读。</div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/7060850" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/703de91b-d594-4d5b-9af9-c2c327212f0e.png" class="cover" />
+                  <div class="name">微信小程序专栏</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">程序员</span>
+                      <span class="category">IT</span>
+                      <span class="category">知识</span>
+                      <span class="category">合集</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      一斤代码
+                    </span>
+                  </div>
+                  <div class="intro twoline"></div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/12703694" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/05d6ee81-273c-470f-91bf-3da8af3b0999.jpg" class="cover" />
+                  <div class="name">正史有意思</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">历史</span>
+                      <span class="category">文化</span>
+                      <span class="category">人物</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      心草雪月
+                    </span>
+                  </div>
+                  <div class="intro twoline">用温软的手指触摸坚硬的化石，用易逝的生命叩问无穷的历史。</div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/14112366" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/77970edf-5354-4077-aacb-27e943d65a41.png" class="cover" />
+                  <div class="name">脑科学科普</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">科普</span>
+                      <span class="category">知识</span>
+                      <span class="category">合集</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      姚脑师
+                    </span>
+                  </div>
+                  <div class="intro twoline"></div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/14861030" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/9ddb109b-38be-40db-b8ff-bc586af7ae77" class="cover" />
+                  <div class="name">揭开护肤化妆品的真相</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">科普</span>
+                      <span class="category">美容化妆</span>
+                      <span class="category">知识</span>
+                      <span class="category">合集</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      莫嫡Morettie
+                    </span>
+                  </div>
+                  <div class="intro twoline">
+                    1)如果我告诉你， 敏感肌肤， 裸脸才能快速修复， 你有何感想？ 2)如果我告诉你，护肤品只有保湿的作用，并没有其他功效，你有何感想？
+                    3)如果我告诉你， 你脸上的痘痘， 很可能是使用产品导致的， 不需要使用祛痘产品也能好，你有何感想？ 4)如果我告诉你， 脸上涂东西有刺痛时，
+                    不是皮肤缺水，而是化学成分刺激到皮肤了， 你有何感想？ ...... 是的， 在这里， 有很多不为人知的秘密， 关于护肤品、化妆品。
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/15982894" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/51acd206-2407-4a54-942e-857966e76dde" class="cover" />
+                  <div class="name">信手涂鸦</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">散文随笔</span>
+                      <span class="category">生活</span>
+                      <span class="category">合集</span>
+                      <span class="category">行距</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      名贵的考拉熊
+                    </span>
+                  </div>
+                  <div class="intro twoline">下笔时的创作态度并不比在B站发送弹幕更严肃2333</div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/20838974" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/ec50f80e-f89e-4a89-8f54-eb3042e56402" class="cover" />
+                  <div class="name">徒步沙场</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">军旅</span>
+                      <span class="category">青春</span>
+                      <span class="category">成长</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      乐健君
+                    </span>
+                  </div>
+                  <div class="intro twoline">
+                    一个从十七岁开始，步入军事院校，涉足自己一片茫然的绿色世界。从坐如钟、行如风、站如松开始，经历过野战部队、守备部队、地方部队的磨练，见证了中国人民解放军从数量规模型向质量效能型转变的大部分过程。把人生最灿烂的美好回忆，也是人生朝气蓬勃的30年，留给了魂牵梦绕直线加方块的绿色军营，留给了维护世界和平，保护国家利益和人民群众生命财产安全的人民军队。
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/21632959" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/d62261c2-2ee3-4dd2-bce7-4997eb6699a3" class="cover" />
+                  <div class="name">骨科病房日记（中篇）</div>
+                  <div class="intro oneline">
+                    <span class="paid">
+                      <i class="iconfont ic-paid iconfufei"></i> 付费
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">医学</span>
+                      <span class="category">生活</span>
+                      <span class="category">付费连载</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      猴子猪
+                    </span>
+                  </div>
+                  <div class="intro twoline">
+                    2018年春节前夕，发生在中心医院骨科病房里的故事。这里有生生死死、悲欢离合，也有脑洞大开、妙语连珠。其中满满的是友情、亲情、爱情，人世间的感情永远是一个让人欲罢不能的话题。正是：雪花飘去又一春，枯叶落尽绿芽新。世间若无真情在，哪有东风暖人心？
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/21665340" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/8e0c2465-ed98-47ab-be93-86991ffd5303.jpg" class="cover" />
+                  <div class="name">三生三世续写–东华凤九</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">同人</span>
+                      <span class="category">古风</span>
+                      <span class="category">言情</span>
+                      <span class="category">三生三世</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      motobndr
+                    </span>
+                  </div>
+                  <div class="intro twoline">一份情，两个人，三世恋，不相离</div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/26337419" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/98937a81-d810-4132-b00d-e0430457525a" class="cover" />
+                  <div class="name">更“无用”的写作课</div>
+                  <div class="intro oneline">
+                    <span class="paid">
+                      <i class="iconfont ic-paid iconfufei"></i> 付费
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      行距版君
+                    </span>
+                  </div>
+                  <div class="intro twoline">
+                    本课程因导师精力有限，现已经停止售卖，感兴趣的简友请戳：https://www.jianshu.com/nb/30459807 ，听导师们的音频课“一部小说的诞生”。
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/26679227" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/20a7b984-0f21-4937-9dc4-9a38677fe772.jpg" class="cover" />
+                  <div class="name">屁话</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">古典</span>
+                      <span class="category">神话</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      假简
+                    </span>
+                  </div>
+                  <div class="intro twoline"></div>
+                </a>
+              </li>
+              <li>
+                <a href="/nb/26877321" class="notebook">
+                  <img src="http://upload.jianshu.io/book/image/a76207ec-cddd-4b18-afd8-f85a5d531c34" class="cover" />
+                  <div class="name">于无声处听梦话</div>
+                  <div class="intro oneline">
+                    <span class="info">
+                      <i class="iconfont ic-tag icontag"></i>
+                      <span class="category">幽默</span>
+                      <span class="category">生活</span>
+                      <span class="category">杂文</span>
+                    </span>
+                    <span class="info">
+                      <i class="iconfont ic-user iconyonghu"></i>
+                      古眼
+                    </span>
+                  </div>
+                  <div class="intro twoline">长夜漫漫，启明姗姗，醒着睡着，谁知为何，不如大家说说梦话……</div>
+                </a>
+              </li>
+            </ol>
+          </div>
+        </div>
       </div>
     );
   }
