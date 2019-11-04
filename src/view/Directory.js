a54027697ec095ea4f0112773e06e262acff66b1
nuochong <nuo.chong@163.com>
2019-11-04 17:04:58 +0800

    update

--- src/view/Directory.js
+++ src/view/Directory.js
@@ -32,7 +32,7 @@ class Article extends Component {
     app.classList.add('v-dialog-mask');
   };
   showModal2 = key => e => {
-    console.log('xxxxxxxx')
+    console.log('xxxxxxxx');
     this.state.modal1 = false;
     e.preventDefault(); // 修复 Android 上点击穿透
     this.setState({
@@ -55,12 +55,12 @@ class Article extends Component {
     let app = document.getElementById('root');
     app.classList.remove('v-dialog-mask');
   };
-  onChange = key =>()=>{
+  onChange = key => () => {
     this.state.modal1 = true;
     this.setState({
       [key]: false
     });
-  }
+  };
 
   onWrapTouchStart = e => {
     // fix touch to scroll background page on iOS
@@ -348,6 +348,33 @@ class Article extends Component {
             </div>
           </div>
         </div>
+        <hr/>
+        {/* 连载作品 */}
+        <div class="note-novel-info">
+          <div class="top-title">连载作品</div>
+          <a href="/nb/23058721" class="">
+            <img
+              src="https://upload.jianshu.io/book/image/741d215c-5eb3-4202-a7c0-2448c74aa494?imageMogr2/auto-orient/strip|imageView2/1/w/180/h/240/format/webp"
+              alt=""
+              class="cover"
+            />
+          </a>
+          <a href="/nb/23058721" class="name">
+            青春玄幻|我的左眼 第一季
+          </a>
+          <button class="follow-btn notebook-follow-button follow">
+            <i class="iconfont ic-follow iconadd"></i>
+            关注
+          </button>
+          <div class="intro">
+            主要人物:黎小奕，左眼先天性眼球发育不完全。(性格倔强，外表冰冷，内心既多愁善感又善良热心。)有非常强烈而准确的第六感。 黎孝天，帅气的邻家小哥哥
+            ，黎小奕的跟屁虫和保护神。为人热情，对小奕死心塌地。 小奕的姆妈，一个六十多岁，几乎可以当她奶奶的驼背寡妇。
+            故事梗概:十六年前，四十多岁的寡妇黎嫂在除夕夜捡到一个弃婴。这个弃婴先天残疾，但是从此被孤独一人的黎嫂当做心肝宝贝，含辛茹苦将她养大。本来就贫困又残疾的两个可怜人，幸亏有邻居黎孝天以及他父母的接应，还有政府的照顾
+            。残疾的女孩有没有资格得到健全人的爱?她能不能带着她的姆妈过上幸福的生活?
+          </div>
+          <div class="meta">122868字 · 14269阅读</div>
+        </div>
+      
         <hr />
         <div class="recommend-novels-list">
           <div class="top-title">
@@ -529,6 +556,8 @@ class Article extends Component {
             </div>
           </main>
         </Modal>
+
+        
       </div>
     );
   }
