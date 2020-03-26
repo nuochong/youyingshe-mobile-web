f34ce49f15e3a01b96573ba63f14fc95d8eebba3
nuochong <nuo.chong@163.com>
2020-03-26 14:55:34 +0800

    update

---
+++ docs/precache-manifest.ebddb48dcde1a3802a8d59dfa66f6d25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238e6c2fb960532a921f16e191ae6e96",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b89dd8f3fa8063cc3836",
+    "url": "./static/css/2.428ea7ce.chunk.css"
+  },
+  {
+    "revision": "124feafa073516ed9637",
+    "url": "./static/css/main.fa1730b0.chunk.css"
+  },
+  {
+    "revision": "b89dd8f3fa8063cc3836",
+    "url": "./static/js/2.a6527086.chunk.js"
+  },
+  {
+    "revision": "124feafa073516ed9637",
+    "url": "./static/js/main.bd8ab91e.chunk.js"
+  },
+  {
+    "revision": "274e6261dc6e238d35c4",
+    "url": "./static/js/runtime~main.05db9c3c.js"
+  }
+]);
