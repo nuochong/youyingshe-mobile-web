92945696ee20f598f47987dad28551a73f2daf52
nuochong <nuo.chong@163.com>
2020-03-26 14:50:00 +0800

    update

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,10 @@
 const { override, fixBabelImports } = require('customize-cra');
+const path = require('path');
+const paths = require('react-scripts/config/paths');
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd-mobile',
     style: 'css'
   })
 );
+paths.appBuild = path.join(path.dirname(paths.appBuild), 'docs'); // 修改打包目录
